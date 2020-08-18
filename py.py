3422bf23bf23de4fd03ec29c267b05886e643031
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-08-18 18:56:30 +0900

    Update py.py

--- py.py
+++ py.py
@@ -16,20 +16,7 @@ async def on_ready():
   
 @client.event
 async def on_message(message):
-  if message.content == "!report":
-    if message.content[8:16] == "Cheating":
-      if message.content[17:]:
-        await client.send_message(message.channel, "Reported by: `" + message.author + "`\nA reported person: `" + message.content[18:] + "`\nReason: `Cheating`")
-    if message.content[8:16] == "Boosting":
-      if message.content[17:]:
-        await client.send_message(message.channel, "Reported by: `" + message.author + "`\nA reported person: `" + message.content[18:] + "`\nReason: `Boosting`")
-    if message.content[8:16] == "Teammate":
-      if message.content[17:]:
-        await client.send_message(message.channel, "Reported by: `" + message.author + "`\nA reported person: `" + message.content[18:] + "`\nReason: `Teammate`")
-  if message.content == "!reasons":
-    await client.send_message(message.channel, "`Reason Types:`\n`Cheating`\n`Boosting`\n`Teammate`")
-  if message.content == "!help":
-    await client.send_message(message.channel, "`Commands:`\n`!report (reason) (username)` - report the player.\n`!reasons` - Report types.")
+
 
 
 access_token = os.environ["BOT_TOKEN"]
