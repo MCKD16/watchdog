bf3148c0377dc6366e363a739cf5fba5d5a248e3
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-08-18 18:43:05 +0900

    Create py.py

---
+++ py.py
@@ -0,0 +1,57 @@
+import discord
+import os
+import datetime
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+  print("login")
+  print(client.user.name)
+  print(client.user.id)
+  print("------------------")
+  await client.change_presence(game=discord.Game(name='!help', type=1))
+  
+@client.event
+async def on_message(message):
+  if message.content == "!report":
+    if message.content[8:16] == "Cheating":
+      if message.content[17:]:
+        embed = discordEmbed(
+          title = 'REPORT SYSTEM',
+          description = 'Reported by: ' + message.author + "\nA reported person: " + message.content[18:] + "\nReason: Cheating",
+          colour = discord.Colour.red()
+        )
+        
+        embed.set_footer(text='')
+        embed.set_image(url='')
+    else if message.content[8:16] == "Boosting":
+      if message.content[17:]:
+        embed = discordEmbed(
+          title = 'REPORT SYSTEM',
+          description = 'Reported by: ' + message.author + "\nA reported person: " + message.content[18:] + "\nReason: Boosting",
+          colour = discord.Colour.red()
+        )
+        
+        embed.set_footer(text='')
+        embed.set_image(url='')
+    else if message.content[8:16] == "Teammate":
+      if message.content[17:]:
+        embed = discordEmbed(
+          title = 'REPORT SYSTEM',
+          description = 'Reported by: ' + message.author + "\nA reported person: " + message.content[18:] + "\nReason: Teammate",
+          colour = discord.Colour.red()
+        )
+        
+        embed.set_footer(text='')
+        embed.set_image(url='')
+  if message.content == "!reasons":
+    await client.send_message(message.channel, "`Reason Types:`\n`Cheating`\n`Boosting`\n`Teammate`")
+  if message.content == "!help":
+    await client.send_message(message.channel, "`Commands:`\n`!report (reason) (username)` - report the player.\n`!reasons` - Report types.")
+
+
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
